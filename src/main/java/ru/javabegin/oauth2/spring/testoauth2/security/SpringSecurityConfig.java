beb9436c1e307ebeaa628fa7713ec94098d09ca9
Pavel Semenov <PNSemenov@sberbank.ru>
2022-08-29 22:53:15 +0300

    added role Converter

--- src/main/java/ru/javabegin/oauth2/spring/testoauth2/security/SpringSecurityConfig.java
+++ src/main/java/ru/javabegin/oauth2/spring/testoauth2/security/SpringSecurityConfig.java
@@ -5,23 +5,35 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.javabegin.oauth2.spring.testoauth2.converter.KCRoleConverter;
 
 @Configuration // данный класс будет считан как конфиг для spring контейнера
 @EnableWebSecurity // включает механизм защиты адресов, которые настраиваются в SecurityFilterChain
-// в старых версиях spring security нужно было наследовать от спец. класса WebSecurityConfigurerAdapter
-// Подробнее https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
-@EnableGlobalMethodSecurity(prePostEnabled = true) //включение механизма для защиты методов по ролям
+@EnableGlobalMethodSecurity(prePostEnabled = true) // включение механизма для защиты методов по ролям
 public class SpringSecurityConfig {
 
     // создается спец. бин, который отвечает за настройки запросов по http (метод вызывается автоматически) Spring контейнером
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+        // конвертер для настройки spring security
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        // подключаем конвертер ролей
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KCRoleConverter());
+
+
         // все сетевые настройки
         http.authorizeRequests()
-                .antMatchers("/test/login").permitAll() // анонимный пользователь сможет выполнять запросы только по этим URI
-                .anyRequest().authenticated(); // остальной API будет доступен только аутентифицированным пользователям
+                .antMatchers("/login").permitAll() // анонимный пользователь сможет выполнять запросы только по этим URI
+                .anyRequest().authenticated() // остальной API будет доступен только аутентифицированным пользователям
+
+                .and() // добавляем новые настройки, не связанные с предыдущими
+
+                .oauth2ResourceServer()// добавляем конвертер ролей из JWT в Authority (Role)
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter);
 
         return http.build();
     }
